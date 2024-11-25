06d2f13297065750ed45542ed486e549c472f3ab
Alutta <Aluttamatambo5@gmail.com>
2024-11-25 21:57:36 +0200

    Added useState & useEffect to fetch data in ShowDetailsPage component

--- src/Components/ShowDetailePage.js
+++ src/Components/ShowDetailePage.js
@@ -5,3 +5,9 @@ import { useParams } from 'react-router-dom';
 const ShowDetailsPage = () => {
     const { id } = useParams();
     const [show, setShow] = useState({});
+
+    useEffect(() => {
+        fetch(`https://podcast-api.netlify.app/genre/${id}`)
+        .then((response) => response.json())
+        .then((data) => setShow (data))
+    })
