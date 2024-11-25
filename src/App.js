a54a59e3448300c2f914af033a05b94cccf74913
Alutta <Aluttamatambo5@gmail.com>
2024-11-25 21:48:19 +0200

    Updated import statement for ShowDetailsPage component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Home from './Components/Home';
-import ShowDetailsPage from './Components/ShowDetailsPage';
+import ShowDetailsPage from './Components/ShowDetailePage';
 import logo from './logo.svg';
 import './App.css';
 
