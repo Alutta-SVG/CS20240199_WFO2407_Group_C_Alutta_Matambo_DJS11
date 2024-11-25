cc0520db5a14f397581ec1518a070e4f9b0dbb7b
Alutta <Aluttamatambo5@gmail.com>
2024-11-25 21:30:45 +0200

    Moved component return statement inside useEffect hook & added export default statement

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -15,22 +15,27 @@ const Home = () => {
         })
         .catch((error) => console.error('Error fetching shows:', error))
         }, []);
+
+        return (
+            <div className="home">
+            <header className = "header">
+                <h1>Podcast App</h1>
+                <input type = "text" placeholder = "Search...." className = " search-bar"></input>
+            </header>
+            <div className = "shows-grid">
+                {shows.map((show) => (
+                    <div key = {show.id} className = "show-card">
+                        <img src ={show.image} alt={show.title} className="show-image"/>
+                        <h3>{show.title}</h3>
+                        <p>{show.description}</p>
+            </div>
+                ))}
+            </div>
+            </div>
+        );
+
     }
 
-    return (
-        <div className="home">
-        <header className = "header">
-            <h1>Podcast App</h1>
-            <input type = "text" placeholder = "Search...." className = " search-bar"></input>
-        </header>
-        <div className = "shows-grid">
-            {shows.map((show) => (
-                <div key = {show.id} className = "show-card">
-                    <img src ={show.image} alt={show.title} className="show-image"/>
-                    <h3>{show.title}</h3>
-                    <p>{show.description}</p>
-        </div>
-            ))}
-        </div>
-        </div>
-    );
+    export default Home;
+
+    
