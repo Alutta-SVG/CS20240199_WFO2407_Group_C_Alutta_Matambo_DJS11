00b33970f3bd613b567d0572d8092b0cd2a212db
Alutta <Aluttamatambo5@gmail.com>
2024-11-27 00:27:56 +0200

    Added NavBar component with basic structure and Home link

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,3 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="nav-list">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
