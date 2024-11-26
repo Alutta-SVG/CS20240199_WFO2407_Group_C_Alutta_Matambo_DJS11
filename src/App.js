e070f72a068b78751e6a81016bc06ce8024e978c
Alutta <Aluttamatambo5@gmail.com>
2024-11-27 01:29:33 +0200

    renamed NavBar to Sidebar, added App.css import, & modified component layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
-import ShowDetailsPage from './Components/ShowDetailePage';
-import NavBar from './Components/sidebar';
-
+import ShowDetailsPage from '../src/Components/ShowDetailePage';
+import Sidebar from './Components/sidebar';
+import './App.css';
 
 const App = () => {
     return (
         <Router>
-            <div>
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/show/:id" element={<ShowDetailsPage />} />
-                </Routes>
+            <div className="app-container">
+                <Sidebar />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/show/:id" element={<ShowDetailsPage />} />
+                    </Routes>
+                </div>
             </div>
         </Router>
     );
 };
- 
+
 export default App;
